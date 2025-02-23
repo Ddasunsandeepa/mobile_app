c45ae45a4faa289e8fcc7b0f03636d4ef1b441dc
John Doe <dsk.sandeep.987@gmail.com>
2025-02-23 21:03:03 +0530

    assets and sandBox updates

--- lib/main.dart
+++ lib/main.dart
@@ -12,37 +12,60 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "new app",
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 247, 225, 159),
-          title: const Text("hello wolrd"),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.notification_add_sharp), // Correct syntax
-              onPressed: () {
-                // Perform an action when the button is pressed
-              },
-            ),
-          ],
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start, // Centers the content
-          children: [
-            const Icon(Icons.add, size: 50, color: Colors.blue),
-            const Text("Add Icon", style: TextStyle(fontSize: 24)),
-            Center(
-              child: Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(50),
-                    color: Colors.indigoAccent),
-                margin: const EdgeInsets.all(10),
-                child: Center(child: Text("hello")),
+          backgroundColor: Colors.black26,
+          appBar: AppBar(
+            backgroundColor: const Color.fromARGB(255, 247, 225, 159),
+            title: const Text("hello wolrd"),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.notification_add_sharp), // Correct syntax
+                onPressed: () {
+                  // Perform an action when the button is pressed
+                },
               ),
-            )
-          ],
-        ),
-      ),
+              IconButton(
+                icon: Icon(Icons.notification_add_sharp), // Correct syntax
+                onPressed: () {
+                  // Perform an action when the button is pressed
+                },
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start, // Centers the content
+              children: [
+                const Icon(Icons.add, size: 50, color: Colors.blue),
+                const Text("Add Icon", style: TextStyle(fontSize: 24)),
+                Center(
+                  child: Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.indigoAccent),
+                    margin: const EdgeInsets.all(10),
+                    child: Center(child: Text("hello")),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Image.asset(
+                      "assets/space1.png",
+                      height: 280,
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Image.asset(
+                      "assets/space2.png",
+                      height: 280,
+                    )
+                  ],
+                )
+              ],
+            ),
+          )),
     );
   }
 }
