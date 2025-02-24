6ebfba58240f18e1b2cf5b2ac088b77e3dbade09
John Doe <dsk.sandeep.987@gmail.com>
2025-02-24 21:48:52 +0530

    develop the frontend UI

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
                 icon: Icon(
                   Icons.menu,
                   color: Colors.white,
+                  size: double.tryParse("30.0"),
                 ),
               )
             ],
@@ -67,23 +68,142 @@ class MyApp extends StatelessWidget {
                 ),
                 const SizedBox(
                   height: 30,
+                  width: 300,
                 ),
                 Center(
                   child: Container(
                     child: Padding(
-                      padding: EdgeInsets.all(14.0),
+                      padding: EdgeInsets.all(12.0),
                       child: Text(
-                        "Click Here",
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
+                        "Space Details",
+                        style: TextStyle(color: Colors.white, fontSize: 17),
                       ),
                     ),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
-                        color: Color.fromARGB(255, 244, 2, 67)),
+                        color: const Color.fromARGB(255, 244, 2, 67)),
                   ),
-                )
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/space2.png",
+                    height: 340,
+                  ),
+                ),
+                const Text(
+                  "Text(String data, {Key? key, TextStyle? style, StrutStyle? strutStyle, TextAlign? textAlign, TextDirection? textDirection, Locale? locale, bool? softWrap, TextOverflow? overflow, double? textScaleFactor, TextScaler? textScaler,StrutStyle?TextOverflow? overflow, double? textScaleFactor, TextScaler? textScaler,StrutStyle?  strutStyle, TextAlign? textAlign, TextDirection? textDirection, textScaleFactor, TextScaler? textScaler, int? maxLines, String? semanticsLabel, TextWidthBasis? textWidthBasis, TextHeightBehavior? TextOverflow? overflow, double? textScaleFactor, TextScaler? textScaler,StrutStyle? ",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w400),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.amber,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Color.fromARGB(255, 255, 0, 0),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 12),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: const Color.fromARGB(255, 181, 7, 255),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: const Color.fromARGB(255, 7, 255, 57),
+                      ),
+                    )
+                  ],
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/space3.png",
+                    height: 300,
+                  ),
+                ),
+                const Text(
+                  "Text(String data, {Key? key, TextStyle? style, StrutStyle? strutStyle, TextAlign? textAlign, TextDirection? textDirection, Locale? locale, bool? softWrap, TextOverflow? overflow, double? textScaleFactor, TextScaler? textScaler,StrutStyle?TextOverflow? overflow, double? textScaleFactor, TextScaler? textScaler, overflow, double? textScaleFactor, TextScaler? textScaler,StrutStyle? ",
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.w400),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: Container(
+                    child: Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text(
+                        "Space Details",
+                        style: TextStyle(color: Colors.black, fontSize: 17),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Color.fromARGB(255, 2, 244, 184)),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Divider(
+                        height: 10,
+                        thickness: 1,
+                        indent: 20,
+                        endIndent: 0,
+                        color: Color.fromARGB(255, 215, 187, 187)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "BLACK HOLE",
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: const Text(
+                    "Text(String data, {Key? key, TextStyle? style, StrutStyle? strutStyle, TextAlign? textAlign, TextDirection? textDirection, Locale? locale, bool? softWrap, TextOverflow? ",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.left,
+                  ),
+                ),
               ],
             ),
           ),
